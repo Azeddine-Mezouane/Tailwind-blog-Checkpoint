a730e7ecd539ff175d83cbb60f667634d2e0cbd9
Azeddine-Mezouane <azeddine.mezouane@gmail.com>
2024-12-18 09:42:00 +0100

    first commit

---
+++ l.js
@@ -0,0 +1,10 @@
+const lenis = new Lenis({
+    autoRaf: true,
+    lerp:0.08
+  });
+  
+  // Listen for the scroll event and log the event data
+  lenis.on('scroll', (e) => {
+    console.log(e);
+  });
+
